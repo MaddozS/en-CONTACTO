66aa99191d785f108a5925d0936ef091c02eebaf
Axel Adrián Anaya García <axel_anaya@hotmail.com>
2019-05-02 07:45:57 -0500

    Cambio de funciones y adición de nuevas

--- main.c
+++ main.c
@@ -10,21 +10,26 @@
 
 //funcion para cambiar de color el texto
 void color(char color);
+void titulo();
 int menu_principal(int opcion);
 void imprimir_menu_principal();
-void titulo();
 void esconder_cursor();
 void centrar_texto(int espacios);
 void ejecutar_opcion_menu_principal(int opcion);
+void crear_pin();
+void verificar_pin();
+void iniciar_sesion();
 
-int posOpcion=0, i, salida=0, key=0;
+int posOpcion=0, i, salida=0, key=0, pinAsignado=0, size, numeroOpciones=3, opcion;
+FILE *pinArchivo;
 
 int main(){
-    int opcion, tecla = key;
+    //funcion para verificar si ya hay un pin generado
+    verificar_pin();
+    //Funcion que imprime las opciones
     opcion = menu_principal_opciones();
-
+    //Funcion ejecuta la opcion seleccionada
     ejecutar_opcion_menu_principal(opcion);
-  
     return 0;
 }
 //funcion para cambiar de opcion seleccionada, usando las flechas del teclado
@@ -39,22 +44,19 @@ int menu_principal_opciones(){
         switch (key){
             case UP:
                 if(posOpcion==0){
-                    posOpcion=3;
+                    posOpcion=numeroOpciones;
                 }
                 else{
                     posOpcion--;
                 }
                 break;
             case DOWN:
-                if(posOpcion==3){
+                if(posOpcion==numeroOpciones){
                     posOpcion=0;
                 }
                 else{
                     posOpcion++;
                 }
-                break;
-            default:
-                posOpcion=posOpcion;
                 break;
         }
     }
@@ -84,30 +86,91 @@ void centrar_texto(int extensionString){
 //funcion para imprimir las opciones del programa
 void imprimir_menu_principal(int opcionSelec){
     //Cambio de color si la opcion esta seleccionada
-    if(opcionSelec==0){color(4);} else{color(7);} 
-    centrar_texto(14);
-    printf("Iniciar sesi%cn\n", 162);
+    if(pinAsignado == 1){
+        if(opcionSelec == numeroOpciones-3){color(4);} else{color(7);} 
+        centrar_texto(15);
+        printf("Iniciar sesi%cn\n", 162);
+        //Cambio de color si la opcion esta seleccionada
+        if(opcionSelec == numeroOpciones-2){color(4);} else{color(7);} 
+        centrar_texto(9);
+        printf("Opci%cn 2\n", 162);
+        //Cambio de color si la opcion esta seleccionada
+        if(opcionSelec == numeroOpciones-1){color(4);} else{color(7);}
+        centrar_texto(9);
+        printf("Opci%cn 3\n", 162);
+    }
+    else{
+        if(opcionSelec == 0){color(4);} else{color(7);} 
+        centrar_texto(10);
+        printf("Crear PIN\n");
+    }
     //Cambio de color si la opcion esta seleccionada
-    if(opcionSelec==1){color(4);} else{color(7);} 
-    centrar_texto(8);
-    printf("Opci%cn 2\n", 162);
-    //Cambio de color si la opcion esta seleccionada
-    if(opcionSelec==2){color(4);} else{color(7);}
-    centrar_texto(8);
-    printf("Opci%cn 3\n", 162);
-    //Cambio de color si la opcion esta seleccionada
-    if(opcionSelec==3){color(4);} else{color(7);}
-    centrar_texto(18);
+    if(opcionSelec == numeroOpciones){color(4);} else{color(7);}
+    centrar_texto(19);
     printf("Salir del programa\n");
 }
+void crear_pin(){
+    char pinUsuario[5];
+    system("cls");
+    pinArchivo = fopen("pin.txt", "w+");
+    color(6);
+    printf("Ingresa PIN con el que vas a proteger tus contactos (de 4 digitos): ");
+    color(7);
+    fflush(stdin);
+    gets(pinUsuario);
+    fputs(pinUsuario, pinArchivo);
+    fflush(pinArchivo);
+    fclose(pinArchivo);
+    printf("\n");
+    centrar_texto(22); printf("PIN Creado con exito!", 130);
+    centrar_texto(54); printf("Pulsa cualquier tecla para regresar al menu principal");
+    //"Regreso" al menu principal
+
+    //funcion para verificar si ya hay un pin generado
+    verificar_pin();
+    //Funcion que imprime las opciones
+    opcion = menu_principal_opciones();
+    //Funcion ejecuta la opcion seleccionada
+    ejecutar_opcion_menu_principal(opcion);
+    
+}
+void verificar_pin(){
+    pinArchivo = fopen("pin.txt", "r");
+    if (pinArchivo != NULL) {
+        fseek (pinArchivo, 0, SEEK_END);
+        size = ftell(pinArchivo);
+        if (size != 0) {
+            pinAsignado=1;
+            numeroOpciones=3;
+        }
+    }
+    else{
+        pinAsignado=0;
+        numeroOpciones=1;
+    }
+    fclose(pinArchivo);
+}
+void iniciar_sesion(){
+    // system("cls");
+    // pin = fopen("pin.txt", "r");
+    // if (pin != NULL){
+    //     printf("No hay ningun pin asignado\n");
+    // }
+    // else{
+    //     printf("no se puede abrir el archivo\n");
+    // }
+}
 void ejecutar_opcion_menu_principal(int opcion){
-    switch (opcion)
-    {
-        case 1:
-        
-        case 4:
-            exit(0);
-            break;
+    if(opcion == 0){
+        if(pinAsignado==1){
+            iniciar_sesion();
+        }
+        else{
+            crear_pin();
+        }
+    }
+    else if (opcion == numeroOpciones){
+        exit(0);
     }
 }
 
